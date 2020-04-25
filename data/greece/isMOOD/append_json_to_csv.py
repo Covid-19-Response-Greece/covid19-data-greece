3271d493b37433931e4aea0c5db8e90261b404e1
Phaedon-Vagionas <62917205+Phaedon-Vagionas@users.noreply.github.com>
2020-04-25 20:30:22 +0300

    Update append_json_to_csv.py

--- data/greece/isMOOD/append_json_to_csv.py
+++ data/greece/isMOOD/append_json_to_csv.py
@@ -7,21 +7,22 @@ from datetime import datetime
 
 REGIONS_JSON_PATH = './regions.json'
 
-CASES_BY_REGION_TIMELINE_CSV_PATH = './cases_by_region_timeline.csv'
+CASES_BY_REGION_TIME_SERIES_CSV_PATH = './cases_by_region_timeline.csv'
 
 
 def append_json_to_csv():
     
-    timeline_df = pd.read_csv(CASES_BY_REGION_TIMELINE_CSV_PATH, encoding = 'utf8', header = 0)
     today = datetime.today().strftime('%d/%m/%Y')
     new_data = pd.read_json(REGIONS_JSON_PATH)
     new_data = new_data.drop('region_gr_name', axis = 1).rename(columns = {'region_cases': today}).set_index('region_en_name')
-    if today not in timeline_df.columns:
-        timeline_df = timeline_df.merge(new_data, left_on = 'Region', right_on = 'region_en_name', how = 'left')
-    else:
-        timeline_df = timeline_df.drop(today, axis = 1).merge(new_data, left_on = 'Region', right_on = 'region_en_name', how = 'left')
+    time_series_df = pd.read_csv(CASES_BY_REGION_TIME_SERIES_CSV_PATH, encoding = 'utf8', header = 0)
+    if today in time_series_df.columns:
+        time_series_df = time_series_df.drop(today, axis =1)
+    time_series_df = time_series_df.merge(new_data, left_on = 'Region', right_on = 'region_en_name', how = 'left')
     
-    return timeline_df.to_csv(CASES_BY_REGION_TIMELINE_CSV_PATH, index = False)
+    return time_series_df.to_csv(CASES_BY_REGION_TIMELINE_CSV_PATH, index = False)
+
 
 if __name__ == '__main__':
-    append_json_to_csv()
+    append_json_to_csv()    
+
