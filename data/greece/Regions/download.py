a91e4a1a973af6dbdaccc80977abc409aef6800a
billmats96 <vmatsouk@auth.gr>
2020-05-15 20:50:29 +0300

    Insert western macedonia deaths data

--- data/greece/Regions/download.py
+++ data/greece/Regions/download.py
@@ -4,21 +4,24 @@ import sys
 
 DOWNLOADS_DIR = './'
 
-out_format = 'csv'
-out_filename = 'western_macedonia_daily_reports'
+out_format = ['csv', 'csv']
+out_filename = ['western_macedonia_daily_reports', 'western_macedonia_deaths']
 
-source_url = 'https://docs.google.com/spreadsheets/d/19sfozGXRp7uOuVl-cgjNzeqi-Sx64PMo6vxoZtdCmIQ/gviz/tq?tqx=out:' + \
-    out_format + '&sheet=' + out_filename
+source_url = ['https://docs.google.com/spreadsheets/d/19sfozGXRp7uOuVl-cgjNzeqi-Sx64PMo6vxoZtdCmIQ/gviz/tq?tqx=out:',
+              'https://docs.google.com/spreadsheets/d/1uUECU0vTQYuZpFIL4-v-tOtbz1a2C4WQAnwStzU1BBY/gviz/tq?tqx=out:']
+
 
 print('Downloading data ...')
 
-filename = os.path.join(DOWNLOADS_DIR, out_filename + '.' + out_format)
-
-try:
-    urllib.request.urlretrieve(source_url, filename)
-except Exception as inst:
-    print(inst)
-    print('Encountered error')
-    sys.exit()
+for i in range(len(out_filename)):
+    
+    filename = os.path.join(DOWNLOADS_DIR, out_filename[i] + '.' + out_format[i])
+    
+    try:
+        urllib.request.urlretrieve(source_url[i] + out_format[i] + '&sheet=' + out_filename[i], filename)
+    except Exception as inst:
+        print(inst)
+        print('Encountered error')
+        sys.exit()
 
 print('Done.')
