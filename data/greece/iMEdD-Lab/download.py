113791cf5ae41729c1f8fce58b554626206a1534
billmats96 <vmatsouk@auth.gr>
2021-01-31 22:35:01 +0200

    regions history download script update

--- data/greece/iMEdD-Lab/download.py
+++ data/greece/iMEdD-Lab/download.py
@@ -55,23 +55,25 @@ def process():
 
     print("Processing data ...")
     last_updated_at = datetime.now().strftime('%Y-%m-%d')
-
+    cumulative_cases_list = []
+    cumulative_deaths_list = []
     for (file, out_file) in zip(filenames, out_filenames):
 
         with open(file, encoding = 'utf-8') as f:
         	file_pd = pd.read_csv(f, date_parser=lambda x: datetime.datetime.strptime(x, '%d/%m/%Y'), encoding="utf-8")
 
         file_pd = file_pd.where(pd.notnull(file_pd), None)
+        file_pd = file_pd[(file_pd['county_normalized']!= "ΕΛΛΑΔΑ") & (file_pd['county_normalized']!= "ΚΡΟΥΑΖΙΕΡΟΠΛΟΙΟ") & (file_pd['Γεωγραφικό Διαμέρισμα']!= "Ελλάδα")]
         columns_count = len(file_pd.columns)
         region_data_df = pd.DataFrame(columns = ['area_gr', 'area_en', 'region_gr', 'region_en', 'geo_department_gr', 'geo_department_en', 'last_updated_at', 'longtitude', 'latitude' , 'population'] + list(file_pd.columns[5:]))
+        if file == "greece_cases_v2.csv": cumulative_cases_list = list(file_pd.iloc[:, columns_count-1])
+        if file == "greece_deaths_v2.csv": cumulative_deaths_list = list(file_pd.iloc[:, columns_count-1])
 
         for i, row in file_pd.iterrows():
 
             area_gr = row['county_normalized']
-            if area_gr in ["ΕΛΛΑΔΑ", "ΚΡΟΥΑΖΙΕΡΟΠΛΟΙΟ"]: continue
             area_en = area_dict[area_gr]
             region_gr = row['Περιφέρεια'].replace('Περιφέρεια ', '')
-            if region_gr == "Ελλάδα": continue
             region_en = region_dict[region_gr]
             geo_department_gr = row['Γεωγραφικό Διαμέρισμα']
             geo_department_en = geo_department_dict[geo_department_gr]
@@ -84,6 +86,12 @@ def process():
 
         if file == "greece_latest.csv":
             region_data_df = region_data_df.drop(['Πρωτεύουσα', 'county_normalized'], axis=1)
+            temp_df = region_data_df
+            temp_df = temp_df.rename(columns={'new_deaths':'total_deaths', 'new_cases':'total_cases'})
+            temp_df['total_deaths'] = cumulative_deaths_list
+            temp_df['total_cases'] = cumulative_cases_list
+            temp_df.to_csv('regions_cumulative.csv', sep=',', encoding='utf-8', index=False)
+
 
         region_data_df.to_csv(out_file, sep=',', encoding='utf-8', index=False)
         os.remove(file)
